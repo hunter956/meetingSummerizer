44dbf7f7c23bb6086cbb9b991233765d07826a9d
hunter956 <gaikwadpruthviraj008@gmail>
2026-05-17 18:06:49 +0530

    new product dto

---
+++ Metting-Summerizer/src/main/java/com/Metting/Metting/Summerizer/model/ProductDto.java
@@ -0,0 +1,61 @@
+package com.Metting.Metting.Summerizer.model;
+
+public class ProductDto {
+    private Long productId;
+    private String productName;
+    private String category;
+    private Double price;
+    private Integer quantity;
+
+    public ProductDto() {
+    }
+
+    public ProductDto(Long productId, String productName, String category,
+                      Double price, Integer quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
