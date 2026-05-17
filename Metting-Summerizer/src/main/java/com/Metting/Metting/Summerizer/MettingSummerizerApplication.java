0ed2007ebc4dd9073ad1d4453f2dcb7665650d11
hunter956 <gaikwadpruthviraj008@gmail>
2026-05-17 17:59:52 +0530

    first commit

---
+++ Metting-Summerizer/src/main/java/com/Metting/Metting/Summerizer/MettingSummerizerApplication.java
@@ -0,0 +1,13 @@
+package com.Metting.Metting.Summerizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MettingSummerizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MettingSummerizerApplication.class, args);
+	}
+
+}
---
+++ Metting-Summerizer/src/test/java/com/Metting/Metting/Summerizer/MettingSummerizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Metting.Metting.Summerizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MettingSummerizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
